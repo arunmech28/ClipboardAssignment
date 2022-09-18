fc564a76e22319f0bdee8827520e0ced7fa7139f
arunpandian <arunpandian2893@gmail.com>
2022-09-18 18:43:26 +0530

    first commit

---
+++ src/main/java/com/org/assignment/selenidewithspringboot/SelenidewithspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.org.assignment.selenidewithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelenidewithspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelenidewithspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/assignment/selenidewithspringboot/SelenidewithspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.assignment.selenidewithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelenidewithspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
